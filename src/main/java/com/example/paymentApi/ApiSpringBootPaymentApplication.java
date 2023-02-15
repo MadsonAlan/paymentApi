05c8fab493d33382c08dab3488dfafc71e6dadbf
Madson Alan <contato.madsonalan@gmail.com>
2023-02-14 21:17:33 -0300

    Inicio do projeto

---
+++ src/main/java/com/example/paymentApi/ApiSpringBootPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBootPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBootPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paymentApi/ApiSpringBootPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paymentApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringBootPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
