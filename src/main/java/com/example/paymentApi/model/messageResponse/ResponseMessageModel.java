5468de82e25806b31af2179079638eaa4b93fc06
Madson Alan <contato.madsonalan@gmail.com>
2023-02-16 00:32:56 -0300

    atualização do response message

--- src/main/java/com/example/paymentApi/model/messageResponse/ResponseMessageModel.java
+++ src/main/java/com/example/paymentApi/model/messageResponse/ResponseMessageModel.java
@@ -1,15 +1,14 @@
 package com.example.paymentapi.model.messageResponse;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class ResponseMessageModel {
-  String message;
+  private String message;
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
 }
